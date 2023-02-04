8d9477c454911c7e1950c8f2337d2d209d33a4d2
lauritsbonde <lauritsbonde@hotmail.dk>
2023-02-04 16:32:03 +0100

    fixed tests

--- test/BrainTest/NeuralNetwork.test.ts
+++ test/BrainTest/NeuralNetwork.test.ts
@@ -4,26 +4,18 @@ import { test, expect, beforeEach } from '@jest/globals';
 let nn: NeuralNetwork;
 
 beforeEach(() => {
-	nn = new NeuralNetwork(2, 4, 2);
+	nn = new NeuralNetwork(2, 4, 4, 2);
 });
 
 //SETUP
 test('NeuralNetwork is setup correctly', () => {
+	expect(nn).toBeInstanceOf(NeuralNetwork);
 	expect(nn.inputNodes).toBe(2);
 	expect(nn.hiddenNodes).toBe(4);
 	expect(nn.outputNodes).toBe(2);
 
-	expect(nn.weightsIH.rows).toBe(4);
-	expect(nn.weightsIH.cols).toBe(2);
-
-	expect(nn.weightsHO.rows).toBe(2);
-	expect(nn.weightsHO.cols).toBe(4);
-
-	expect(nn.hiddenBias.rows).toBe(4);
-	expect(nn.hiddenBias.cols).toBe(1);
-
-	expect(nn.outputBias.rows).toBe(2);
-	expect(nn.outputBias.cols).toBe(1);
+	expect(nn.weights.length).toBe(5); // 4 hidden layers + 1 output layer
+	expect(nn.biases.length).toBe(5); // 4 hidden layers + 1 output layer
 });
 
 test('NeuralNetwork feedForward', () => {
